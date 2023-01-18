f721fe045420438765834120f295032fa61ab3ba
Danil <danylo_naidonov@epam.com>
2023-01-18 21:55:02 +0200

    js dom

--- index.js
+++ index.js
@@ -1,98 +1,37 @@
-let ID = 1;
-
-const enrollees = [
-   {
-      name: 'Vasya',
-      surname: 'Pyatochkin',
-      schoolPoints: 600,
-      isPrivileges: true,
-   },
-   {
-      name: 'Katya',
-      surname: 'Kurochkina',
-      schoolPoints: 999,
-      isPrivileges: false,
-   },
-   {
-      name: 'Igor',
-      surname: 'Svyatoslavskiy',
-      schoolPoints: 750,
-      isPrivileges: true,
-   },
-   {
-      name: 'Anton',
-      surname: 'Marchenko',
-      schoolPoints: 1340,
-      isPrivileges: false,
-   },
-   {
-      name: 'Maria',
-      surname: 'Ivanenko',
-      schoolPoints: 1000,
-      isPrivileges: false,
-   },
-   {
-      name: 'Svetlana',
-      surname: 'Derevko',
-      schoolPoints: 999,
-      isPrivileges: false,
-   },
-   {
-      name: 'Anton',
-      surname: 'Chorniy',
-      schoolPoints: 0,
-      isPrivileges: false,
-   },
-];
-
-const Student = function(enrollee) {
-   this.id = ID++;
-
-   this.name = enrollee.name;
-   this.surname = enrollee.surname;
-   this.schoolPoints = enrollee.schoolPoints;
-   this.isPrivileges = enrollee.isPrivileges;
-
-   this.isAccepted = null;
-
-   Object.defineProperty(this, 'fullName', {
-      get(){
-         return `${this.surname} ${this.name}`
-      }
-   })
-}
-// Create array with Student instances and return him
-const createStudentsList = (enrolleesList) => {
-   const studentsList = [];
-
-   for(const enrollee of enrolleesList) {
-      const student = new Student(enrollee);
-      studentsList.push(student);
-   }
-
-   return studentsList;
-} 
-
-const students = createStudentsList(enrollees);
-
-const checkOnIsAcceptedStudent = (student) => {
-   if(student.isPrivileges) {
-      return true;
-   }
-
-   const MAX_POINTS = 1000;
-
-   return student.schoolPoints >= MAX_POINTS;
-
+const containerElem = document.querySelector('.container');
+const modalElem = document.querySelector('.modal');
+const modalContainer = document.querySelector('.innerModal');
+modalElem.onclick = (e) => {
+    console.log(e)
+    modalElem.classList.remove('modal_active');
 }
 
-const filterAcceptedStudents = () => {
-   for(const student of students) {
-      student.isAccepted = checkOnIsAcceptedStudent(student);
-   }
+const renderCard = (condidate) => {
+    const cardElem = document.createElement('div');
+    cardElem.className = 'card';
+    cardElem.onclick = () => {
+        modalContainer.innerHTML = Object
+            .entries(condidate)
+            .map(([key, value]) => `<p>${key}: ${value}</p>`)
+            .join(', ');
+        modalElem.classList.add('modal_active')
+        cardElem.classList.toggle('active');
+    };
+
+    const nameElem = document.createElement('p');
+    nameElem.innerText = 'Name: ' + condidate.name;
+    cardElem.append(nameElem);
+
+    const emailElem = document.createElement('p');
+    emailElem.innerText = 'Email: ' + condidate.email;
+    cardElem.append(emailElem);
+
+    return cardElem;
 }
 
-filterAcceptedStudents();
+const renderCards = (condidates) => {
+    const cardsCollection = condidates.map(renderCard)
+    containerElem.append(...cardsCollection)
+}
 
-console.table(students)
-
+renderCards(condidateArr)
